51c4c9c916359423e10858c8055913b3f267f4fe
Adam O'Neil <adamosoftware@gmail.com>
2020-02-18 09:38:53 -0500

    added some DataModel static create methods

--- ModelSync.Library/Models/DataModel.cs
+++ ModelSync.Library/Models/DataModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using ModelSync.Library.Services;
 using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace ModelSync.Library.Models
 {
@@ -8,5 +12,23 @@ namespace ModelSync.Library.Models
         public IEnumerable<Schema> Schemas { get; set; }
         public IEnumerable<Table> Tables { get; set; }
         public IEnumerable<ForeignKey> ForeignKeys { get; set; }
+
+        public static async Task<DataModel> FromSqlServerAsync(IDbConnection connection)
+        {
+            var sqlServer = new SqlServerModelBuilder(connection as SqlConnection);
+            return await sqlServer.GetDataModelAsync();
+        }
+
+        public static async Task<DataModel> FromAssemblyAsync(Assembly assembly, string defaultSchema = "dbo", string defaultIdentityColumn = "Id")
+        {
+            var builder = new AssemblyModelBuilder(assembly, defaultSchema, defaultIdentityColumn);
+            return await builder.GetDataModelAsync();
+        }
+
+        public static async Task<DataModel> FromAssemblyAsync(string fileName, string defaultSchema = "dbo", string defaultIdentityColumn = "Id")
+        {
+            var assembly = Assembly.LoadFrom(fileName);
+            return await FromAssemblyAsync(assembly, defaultSchema, defaultIdentityColumn);
+        }
     }
 }
