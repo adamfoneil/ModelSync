186ad381fef7e6f9b32baed5dda54b43c0eda1fb
Adam O'Neil <adamosoftware@gmail.com>
2020-02-22 11:04:09 -0500

    added Sample Model access

---
+++ Testing/Reflection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelSync.Library.Services;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Testing
+{
+    [TestClass]
+    public class Reflection
+    {
+        [TestMethod]
+        public void LoadSampleModel()
+        {
+            var asm = Assembly.LoadFile(@"C:\Users\Adam\Source\Repos\ModelSync.WinForms\SampleModel\bin\Debug\netstandard2.0\SampleModel.dll");
+            var model = new AssemblyModelBuilder().GetDataModel(asm);
+        }
+    }
+}
