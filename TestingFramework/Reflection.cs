934b9f8b003f8028ea8641e6c36b6b1aa6e8c782
Adam O'Neil <adamosoftware@gmail.com>
2020-02-22 10:02:45 -0500

    not working

--- TestingFramework/Reflection.cs
+++ TestingFramework/Reflection.cs
@@ -13,6 +13,7 @@ namespace TestingFramework
         public void LoadGinsengModels()
         {
             // note: AssemblyHelper is not in the source of this repo -- this test is internal only
+            //Assembly.Load("System.ComponentModel.Annotations, Version=4.2.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
             AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += AssemblyHelper.LoadReflectionOnlyDependencies;
             var assembly = Assembly.ReflectionOnlyLoadFrom(@"C:\Users\Adam\Source\Repos\Ginseng8\Ginseng8.Models\bin\Debug\netstandard2.0\Ginseng.Models.dll");
             var dataModel = new AssemblyModelBuilder().GetDataModel(assembly);
