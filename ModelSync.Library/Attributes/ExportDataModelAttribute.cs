7ad993c62a166d2e4ac91f8b65ba29c57e1fe170
Adam O'Neil <adamosoftware@gmail.com>
2020-03-05 10:06:56 -0500

    added ExpotDataModel attribute

---
+++ ModelSync.Library/Attributes/ExportDataModelAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ModelSync.Library.Attributes
+{
+    /// <summary>
+    /// indicates that you'd like to export this assembly's data model to json during app startup
+    /// to help ModelSync when an assembly can't be loaded dynamically
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+    public class ExportDataModelAttribute : Attribute
+    {
+        public ExportDataModelAttribute(string defaultSchema = "dbo", string defaultIdentityColumn = "Id")
+        {
+            DefaultSchema = defaultSchema;
+            DefaultIdentityColumn = defaultIdentityColumn;
+        }
+
+        public string DefaultSchema { get; }
+        public string DefaultIdentityColumn { get; }
+    }
+}
--- ModelSync.Library/Extensions/AssemblyExtensions.cs
+++ ModelSync.Library/Extensions/AssemblyExtensions.cs
@@ -1,22 +1,34 @@
-﻿using ModelSync.Library.Services;
+﻿using ModelSync.Library.Attributes;
+using ModelSync.Library.Services;
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Reflection;
 
 namespace ModelSync.Library.Extensions
 {
     public static class AssemblyExtensions
-    {
-        /// <summary>
-        /// Use this in your app's startup to export a data model to json at your assembly's location.
-        /// This is handy when your assembly won't load via reflection
-        /// </summary>        
-        public static void ExportDataModel(this Assembly assembly, string defaultSchema = "dbo", string defaultIdentityColumn = "Id")
+    {        
+        private static void ExportDataModel(Assembly assembly, string defaultSchema, string defaultIdentityColumn)
         {
             var dataModel = new AssemblyModelBuilder().GetDataModel(assembly, defaultSchema, defaultIdentityColumn);
             string json = JsonConvert.SerializeObject(dataModel, Formatting.Indented);
             string outputFile = Path.Combine(Path.GetDirectoryName(assembly.Location), Path.GetFileNameWithoutExtension(assembly.Location) + ".DataModel.json");
             File.WriteAllText(outputFile, json);
+        }
+
+        /// <summary>
+        /// Use this in your app's startup, bound to AppDomain.CurrentDomain.AssemblyLoad event to export data models
+        /// from assemblies marked with the [ExportDataModel] attribute. This enables ModelSync to work around issues
+        /// loading assemblies dynamically
+        /// </summary>        
+        public static void ExportDataModel(object sender, AssemblyLoadEventArgs e)
+        {
+            var attr = e.LoadedAssembly.GetCustomAttribute<ExportDataModelAttribute>();
+            if (attr != null)
+            {
+                ExportDataModel(e.LoadedAssembly, attr.DefaultSchema, attr.DefaultIdentityColumn);
+            }
         }
     }
 }
