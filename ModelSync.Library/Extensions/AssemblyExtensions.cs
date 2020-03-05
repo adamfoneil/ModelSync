78e942cddd0c2b3d37674797b4fc90326b182756
Adam O'Neil <adamosoftware@gmail.com>
2020-03-05 09:38:36 -0500

    started on ExportDataModel helper

---
+++ ModelSync.Library/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ModelSync.Library.Services;
+using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+
+namespace ModelSync.Library.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Use this in your app's startup to export a data model to json at your assembly's location.
+        /// This is handy when your assembly won't load via reflection
+        /// </summary>        
+        public static void ExportDataModel(this Assembly assembly, string defaultSchema = "dbo", string defaultIdentityColumn = "Id")
+        {
+            var dataModel = new AssemblyModelBuilder().GetDataModel(assembly, defaultSchema, defaultIdentityColumn);
+            string json = JsonConvert.SerializeObject(dataModel, Formatting.Indented);
+            string outputFile = Path.Combine(Path.GetDirectoryName(assembly.Location), Path.GetFileNameWithoutExtension(assembly.Location) + ".DataModel.json");
+            File.WriteAllText(outputFile, json);
+        }
+    }
+}
--- TestConsoleFramework/Program.cs
+++ TestConsoleFramework/Program.cs
@@ -20,6 +20,7 @@ namespace TestConsoleFramework
             // note: AssemblyHelper is not in the source of this repo -- this test is internal only
             //Assembly.Load("System.ComponentModel.Annotations, Version=4.2.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
             AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += AssemblyHelper.LoadReflectionOnlyDependencies;
+            //System.ComponentModel.Annotations, Version=4.2.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
             var assembly = Assembly.ReflectionOnlyLoadFrom(@"C:\Users\Adam\Source\Repos\Ginseng8\Ginseng8.Models\bin\Debug\netstandard2.0\Ginseng.Models.dll");
             var dataModel = new AssemblyModelBuilder().GetDataModel(assembly);
 
