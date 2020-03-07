4d6d3814543e306a037191b7e68b7196da817f44
Adam O'Neil <adamosoftware@gmail.com>
2020-03-07 17:03:38 -0500

    started on embedded test case, but TestCase deserialization broken due to abstract DbObject

---
+++ Testing/EmbeddedCases.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelSync.Library.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using Testing.Models;
+
+namespace Testing
+{
+    [TestClass]
+    public class EmbeddedCases
+    {
+        [TestMethod]
+        public void TestAllSamples()
+        {
+            var embedded = Assembly.GetExecutingAssembly().GetManifestResourceNames().Where(name => Path.GetExtension(name).Equals(".zip"));
+
+            foreach (var resourceName in embedded)
+            {
+                TestCase testCase = ExtractTestCase(resourceName);
+                var diff = DataModel.Compare(testCase.SourceModel, testCase.DestModel);
+                Assert.IsTrue(diff.SequenceEqual(testCase.DiffActions));
+            }
+        }
+
+        private TestCase ExtractTestCase(string resourceName)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                using (var zip = new ZipArchive(stream, ZipArchiveMode.Read))
+                {
+                    var entry = zip.Entries.First();
+                    using (var entryStream = entry.Open())
+                    {
+                        using (var reader = new StreamReader(entryStream))
+                        {
+                            string json = reader.ReadToEnd();
+                            return JsonConvert.DeserializeObject<TestCase>(json);
+                        }                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Testing/Models/TestCase.cs
@@ -0,0 +1,14 @@
+﻿using ModelSync.Library.Models;
+using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public class TestCase
+    {
+        public DataModel SourceModel { get; set; }
+        public DataModel DestModel { get; set; }
+        public List<ScriptAction> DiffActions { get; set; }
+        public bool IsCorrect { get; set; }
+        public string Comments { get; set; }
+    }
+}
