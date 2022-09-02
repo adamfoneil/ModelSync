b70d6b467a56cf68b3a4ddfadd9e8227e518d1f2
Prabhu <35205779+desirablecoder@users.noreply.github.com>
2022-09-02 16:57:02 +0530

    Add files via upload
    ModelMapAttribute.cs - an attribute for Configuration based model mapping.
    IdentityAttribute.cs - its just a duplicate attribute. it will be moved to AO.Model.IdentityAttribute eventually.

---
+++ ModelSync.Library/Attributes/IdentityAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ModelSync.Library.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class IdentityAttribute : Attribute
+    {
+        public IdentityAttribute(string propertyName, int seed, int increment)
+        {
+            PropertyName = propertyName;
+            Seed = seed;
+            Increment = increment;
+        }
+
+        public string PropertyName { get; }
+        public int Seed { get; }
+        public int Increment { get; }
+    }
+}
---
+++ ModelSync.Library/Attributes/ModelMapAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ModelSync.Library.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ModelMapAttribute : Attribute
+    {
+        public Type Type { get; set; }
+        public ModelMapType ModelMapType { get; set; }
+        public ModelMapAttribute(ModelMapType modelMapType, Type T)
+        {
+            ModelMapType = modelMapType;
+            Type = T;
+        }
+        public ModelMapAttribute(ModelMapType modelMapType)
+        {
+            ModelMapType = modelMapType;
+        }
+    }
+    public enum ModelMapType
+    {
+        Attribute,
+        Config,
+    }
+}
