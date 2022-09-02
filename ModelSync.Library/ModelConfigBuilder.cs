8abdad207ccbf777dd472bb84ec756b3e2611463
Prabhu <35205779+desirablecoder@users.noreply.github.com>
2022-09-02 16:54:33 +0530

    Add files via upload
    this is how i intended to implement method chaining to facilitate configuration based model mapping.

---
+++ ModelSync.Library/ModelConfigBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelSync.Library.Services
+{
+    public class ModelConfigBuilder<T> where T : new()
+    {
+        private T CurrentModel { get; set; }
+        public string IdentityColumn { get; set; }
+        public int IdentitySeed { get; set; }
+        public int IdentityIncrement { get; set; }
+
+        public ModelConfigBuilder()
+        {
+            CurrentModel = GetObject();
+        }
+
+        protected T GetObject()
+        {
+            return new T();
+        }
+        public ModelConfigBuilder<T> AddIdentityColumn(string columnName, int seed, int increment)
+        {
+            IdentityColumn = columnName;
+            IdentitySeed = seed;
+            IdentityIncrement = increment;
+            return this;
+        }
+        //public ModelBuilder<T> Build()
+        //{
+        //    return new ModelBuilder(this);
+        //}
+    }
+}
