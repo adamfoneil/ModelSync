12b5db6853eda869a0a106f23b4d63dffbe2b5cb
Adam O'Neil <adamosoftware@gmail.com>
2022-09-13 08:03:26 -0400

    stubbed out some test code

---
+++ Testing/AssemblySources.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelSync.Models;
+using ModelSync.Services;
+
+namespace Testing
+{
+    [TestClass]
+    public class AssemblySources
+    {
+        [TestMethod]
+        public void Netstandard()
+        {
+            var a = GetAssembly("Hs5.Database.dll");
+            var builder = new AssemblyModelBuilder();
+            var model = builder.GetDataModel(a);
+            var output = DataModel.FromJson("Hs5Model.json");
+            Assert.IsTrue(model.Equals(output));
+        }
+
+        [TestMethod]
+        public void Framework()
+        {
+        }
+
+        [TestMethod]
+        public void Core()
+        { 
+        }
+    }
+}
