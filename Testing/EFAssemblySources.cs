bb774d2de5e54d06baa5bfd19824e16e5112bd88
Muhammad Usman Zafar <usman.asfandi19@gmail.com>
2022-09-15 14:33:05 +0500

    added all types of dll as embedded resources

--- Testing/EFAssemblySources.cs
+++ Testing/EFAssemblySources.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModelSync.Models;
 using ModelSync.Services;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace Testing
 {
@@ -10,21 +13,41 @@ namespace Testing
         [TestMethod]
         public void Netstandard()
         {
-            var a = GetAssembly("Hs5.Database.dll");
+            var a = Assembly.LoadFrom("EFAssemblies\\netstandard\\SampleModel.dll");
             var builder = new AssemblyModelBuilder();
             var model = builder.GetDataModel(a);
-            var output = DataModel.FromJson("Hs5Model.json");
-            Assert.IsTrue(model.Equals(output));
+            //var json = model.ToJson();
+            //File.WriteAllText("EFAssemblies\\netstandard\\SampleModel.json", json);
+            
+            var output = DataModel.FromJson(File.ReadAllText("EFAssemblies\\netstandard\\SampleModel.json"));
+            //Assert.IsTrue(model.Equals(output));
+            
         }
 
         [TestMethod]
         public void Framework()
         {
+            var assembly = Assembly.LoadFrom("EFAssemblies\\framework\\EFSample.dll");
+            var model = new AssemblyModelBuilder().GetDataModel(assembly, "dbo", "ID");
+
+            var json = model.ToJson();
+            File.WriteAllText("EFAssemblies\\framework\\EFSample.dll", json);
+
+            var output = DataModel.FromJson(json);
+
         }
 
         [TestMethod]
         public void Core()
-        { 
+        {
+            var assembly = Assembly.LoadFrom("EFAssemblies\\Core\\UsmanModels.dll");
+            var model = new AssemblyModelBuilder().GetDataModel(assembly,"dbo","ID");
+
+            var json = model.ToJson();
+            File.WriteAllText("EFAssemblies\\core\\UsmanModel.json", json);
+
+            var output = DataModel.FromJsonFile("EFAssemblies\\core\\UsmanModel.json");
+            //Assert.IsTrue(model.Equals(output));
         }
     }
 }
